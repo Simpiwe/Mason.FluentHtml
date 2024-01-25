dd89a9181e049a5a3cf554016d8ae645de1f1ed3
Simpiwe <simpiwe@jagmethod.com>
2024-01-25 07:41:56 +0200

    Added a test that tests nested element logic

--- main/tests/FluentHtml.Tests/HtmlBuilderTests.cs
+++ main/tests/FluentHtml.Tests/HtmlBuilderTests.cs
@@ -53,6 +53,28 @@ namespace FluentHtml.Tests
             Assert.Matches(_nonSelfClosingRegex, document.DocumentNode.FirstChild.OuterHtml);
         }
 
+        [Theory]
+        [InlineData("div", "div")]
+        [InlineData("head", "div")]
+        [InlineData("html", "span")]
+        [InlineData("select", "option")]
+        [InlineData("figure", "figcaption")]
+        public void Element_AddsNestedElement(string parentTag, string tag)
+        {
+            //Arrange
+            HtmlBuilder sut = new();
+            HtmlDocument document = new();
+
+            //Act
+            string html = sut.Element(parentTag, parent => parent.Element(tag)).ToString();
+            document.LoadHtml(html);
+
+            //Assert
+            Assert.Empty(document.ParseErrors);
+            Assert.Single(document.DocumentNode.ChildNodes);
+            Assert.Equal(parentTag, document.DocumentNode.FirstChild.Name);
+            Assert.Equal(tag, document.DocumentNode.FirstChild.FirstChild.Name);
+        }
 
         [Theory]
         [InlineData("img")]
